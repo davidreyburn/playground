f5b8ef4d98c795e2116bb70750ab2a99ff943165
David Reyburn <dreyburn@Davids-MacBook-Pro.local>
2018-08-15 00:18:18 -0400

    initial commit w/ test code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(400, 400);
-}
+var y = 100;
 
+// The statements in the setup() function
+// execute once when the program begins
+function setup() {
+  createCanvas(windowWidth, windowHeight);  // Size must be the first statement
+  stroke(255);     // Set line drawing color to white
+  frameRate(30);
+}
+// The statements in draw() are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
+// line is executed again.
 function draw() {
-  background(220);
+  background(0);   // Set the background to black
+  y = y - 1;
+  if (y < 0) {
+    y = height;
+  }
+  line(0, y, width, y);
 }
