3a3ea9c4aa4ed20e6d1186923e5ee498ef8d3844
David Reyburn <reyburn.david@gmail.com>
2018-08-18 17:00:30 -0400

    horizon grid function

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var colorSwitch = 0;
 var lines = 0;
+var lineCount = 16;
 var paletteCount = 0;
 
 function preload() {
@@ -11,7 +12,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);  // Size must be the first statement
   frameRate(30);
-  lines = [height-1, height-36, height-71, height-106, height-141, height-176, height-211];
+  lines = [...Array(lineCount).keys()].map(i => height*.71 + (i*i));
 }
 // The statements in draw() are executed until the
 // program is stopped. Each statement is executed in
@@ -22,16 +23,17 @@ function draw() {
   fill(palettes[colorSwitch].colors[3]);
   noStroke();
   ellipse(width*.85, height*.2, height*.4, height*.4);
-
-  for (i=0;i<7;i++) {
-    lines[i]++;
+  for (i=0;i<lineCount;i++) {
+    var distance =  lines[i] - (height*.71);
+    lines[i] = lines[i] + 1+abs(distance)/80;
     if (lines[i] > height) {
       lines[i] = height*.71;
     }
   }
 
   stroke(palettes[colorSwitch].colors[0]);
-  for (i = 0; i < 7; i++) {
+  line(0, height*.71, width, height*.71);
+  for (i = 0; i < lineCount; i++) {
     line(0, lines[i], width, lines[i]);
   }
 }
@@ -41,3 +43,12 @@ function mouseReleased() {
       colorSwitch = 0;
     } else colorSwitch++;
   }
+
+function lfo(theta,amplitude) {
+    var x = sin(theta) * amplitude;
+    theta = theta + 0.01;
+    if (theta > 2 * PI) {
+      theta = 0.0;
+    }
+    return abs(x);
+}
